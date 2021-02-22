b085cbbcebf08922372a91d472ccb7f140e1789b
Sam Smith <samuel.smith510@gmail.com>
2021-02-22 01:08:32 -0500

    Trying to be a grownup

--- src/main/java/example/FuzzDriver.java
+++ src/main/java/example/FuzzDriver.java
@@ -1,12 +1,27 @@
+//good
 import edu.berkeley.cs.jqf.fuzz.Fuzz;
 import edu.berkeley.cs.jqf.fuzz.JQF;
 import org.junit.Assume;
 import org.junit.runner.RunWith;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.File;
+import java.io.FileInputStream;
 
 @RunWith(JQF.class)
 public class FuzzDriver {
+
+    public static void main(String[] args) {
+	FuzzDriver me = new FuzzDriver();
+	FileInputStream fis = null;
+	try {
+	    File inputFile = new File(args[1]);
+	    fis = new FileInputStream(inputFile);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	me.fuzz(fis);
+    }
 
     @Fuzz /* JQF will generate inputs to this method */
     public void fuzz(InputStream input)  {
