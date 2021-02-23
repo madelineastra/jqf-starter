b4c087fc77dc7079dd6f7cda37e59bb4e9bf393a
Madeline Smith <maddie.smith@oracle.com>
2021-02-23 11:04:05 -0800

    Cleaned up some scripts

--- fuzz.sh
+++ fuzz.sh
@@ -1,1 +1,7 @@
+export PROJECT_DIR=/home/user/Projects/public
+export AFL_HOME=$PROJECT_DIR/AFLplusplus
+export AFL_DIR=$AFL_HOME
+export JQF_HOME=$PROJECT_DIR/jqf
+export PATH=$PATH:$AFL_HOME:$JQF_HOME/bin
+
 jqf-afl-fuzz -c ./target/jqf-starter-1.0-SNAPSHOT-jar-with-dependencies.jar -i inputs -o outputs FuzzDriver fuzz
